2a94bfab457f3ac8c00dbd90f84f5c7d57c0dc53
nashworth <ashworth.n@gmail.com>
2017-08-23 15:36:57 -0500

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,31 +1,43 @@
 $(document).ready(function () {
   var timeData = [],
-    temperatureData = [],
-    humidityData = [];
+  Acc100Data = [];
+  Acc800Data = [];
+  Acc3200Data = [];
   var data = {
     labels: timeData,
     datasets: [
       {
         fill: false,
-        label: 'Temperature',
-        yAxisID: 'Temperature',
+        label: 'Acc100',
+        yAxisID: 'Amplitude',
         borderColor: "rgba(255, 204, 0, 1)",
         pointBoarderColor: "rgba(255, 204, 0, 1)",
         backgroundColor: "rgba(255, 204, 0, 0.4)",
         pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
         pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-        data: temperatureData
+        data: Acc100Data
       },
       {
         fill: false,
-        label: 'Humidity',
-        yAxisID: 'Humidity',
+        label: 'Acc800',
+        yAxisID: 'Amplitude',
+        borderColor: "rgba(100, 100, 100, 1)",
+        pointBoarderColor: "rgba(100, 100, 100, 1)",
+        backgroundColor: "rgba(100, 100, 100, 0.4)",
+        pointHoverBackgroundColor: "rgba(100, 100, 100, 1)",
+        pointHoverBorderColor: "rgba(100, 100, 100, 1)",
+        data: Acc800Data
+      },
+      {
+        fill: false,
+        label: 'Acc3200',
+        yAxisID: 'Amplitude',
         borderColor: "rgba(24, 120, 240, 1)",
         pointBoarderColor: "rgba(24, 120, 240, 1)",
         backgroundColor: "rgba(24, 120, 240, 0.4)",
         pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
         pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-        data: humidityData
+        data: Acc3200Data
       }
     ]
   }
@@ -33,27 +45,20 @@ $(document).ready(function () {
   var basicOption = {
     title: {
       display: true,
-      text: 'Temperature & Humidity Real-time Data',
+      text: 'Vibration Amplitude Real-time Data',
       fontSize: 36
     },
     scales: {
       yAxes: [{
-        id: 'Temperature',
+        id: 'Amplitude',
         type: 'linear',
         scaleLabel: {
-          labelString: 'Temperature(C)',
+          labelString: 'RMS Amplitude',
           display: true
         },
         position: 'left',
-      }, {
-          id: 'Humidity',
-          type: 'linear',
-          scaleLabel: {
-            labelString: 'Humidity(%)',
-            display: true
-          },
-          position: 'right'
-        }]
+      }, 
+      ]
     }
   }
 
@@ -74,24 +79,31 @@ $(document).ready(function () {
     console.log('receive message' + message.data);
     try {
       var obj = JSON.parse(message.data);
-      if(!obj.time || !obj.temperature) {
+      if(obj.device_id != 420027000d47353136383631)
+      {
         return;
       }
-      timeData.push(obj.time);
-      temperatureData.push(obj.temperature);
+      timeData.push(obj.published_at);
+      Acc100Data.push(obj.Acc_100);
       // only keep no more than 50 points in the line chart
       const maxLen = 50;
       var len = timeData.length;
       if (len > maxLen) {
         timeData.shift();
-        temperatureData.shift();
+        Acc100Data.shift();
       }
 
-      if (obj.humidity) {
-        humidityData.push(obj.humidity);
+      if (obj.Acc_3200) {
+        Acc3200Data.push(obj.Acc_3200);
       }
-      if (humidityData.length > maxLen) {
-        humidityData.shift();
+      if (Acc3200Data.length > maxLen) {
+        Acc3200Data.shift();
+      }
+      if (obj.Acc_800) {
+        Acc800Data.push(obj.Acc_800);
+      }
+      if (Acc800Data.length > maxLen) {
+        Acc800Data.shift();
       }
 
       myLineChart.update();
